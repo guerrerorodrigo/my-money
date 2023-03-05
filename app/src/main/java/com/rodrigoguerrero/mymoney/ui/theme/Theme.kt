dbf820e6b82d643ab8c6f49659ee3f26c2e17131
Rodrigo Guerrero <rodrigo.guerreromulia@jumbo.com>
2023-03-05 15:47:52 +0100

    Material 3 theme.

--- app/src/main/java/com/rodrigoguerrero/mymoney/ui/theme/Theme.kt
+++ app/src/main/java/com/rodrigoguerrero/mymoney/ui/theme/Theme.kt
@@ -1,44 +1,68 @@
 package com.rodrigoguerrero.mymoney.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import com.rodrigoguerrero.mymoney.ui.theme.colors.DarkColors
+import com.rodrigoguerrero.mymoney.ui.theme.colors.LightColors
+import com.rodrigoguerrero.mymoney.ui.theme.colors.LocalMyMoneyColors
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+object MyNotesTheme {
+    val color: ColorScheme
+        @Composable
+        get() = LocalMyMoneyColors.current
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    val padding: MyMoneyPadding
+        @Composable
+        get() = LocalMyMoneyPadding.current
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+    val typography: Typography
+        @Composable
+        get() = LocalMyMoneyTypography.current
+
+    val shapes: Shapes
+        @Composable
+        get() = LocalMyMoneyShapes.current
+}
 
 @Composable
-fun MyMoneyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
+fun MyMoneyTheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (!useDarkTheme) {
+        LightColors
     } else {
-        LightColorPalette
+        DarkColors
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    val padding = MyMoneyPadding()
+    val typography = typography()
+
+    CompositionLocalProvider(
+        LocalMyMoneyColors provides colors,
+        LocalMyMoneyPadding provides padding,
+        LocalMyMoneyTypography provides typography,
+        LocalMyMoneyShapes provides Shapes
+    ) {
+        MaterialTheme(
+            colorScheme = colors,
+            shapes = Shapes,
+            typography = typography
+        ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                content()
+            }
+        }
+    }
 }

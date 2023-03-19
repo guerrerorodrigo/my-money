e1440c71110d73bc374799b3bc5cbee75567a871
Rodrigo Guerrero <rodrigo.guerreromulia@jumbo.com>
2023-03-19 17:15:52 +0100

    Add domain module.

---
+++ domain/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = AppConfig.domainPackage
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(Android.coreKtx)
+
+    implementation(Android.Hilt.android)
+    kapt(Android.Hilt.androidCompiler)
+    kapt(Android.Hilt.compiler)
+
+    testImplementation(Dependencies.Testing.jUnit)
+
+    androidTestImplementation(Dependencies.Testing.androidJUnit)
+    androidTestImplementation(Dependencies.Testing.espresso)
+}
